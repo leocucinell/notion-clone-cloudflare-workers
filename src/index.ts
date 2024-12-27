a0ba88e27a7b3a2ca7bb507014986a18b2d142e7
leo cucinell <leocucinell@gmail.com>
2024-12-27 14:06:35 -0800

    add: custom translation api

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	OPEN_AI_KEY: OpenAI;
+	AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['Content-Type', 'X-Custom-Header', 'Upgrade-Insecure-Requests'],
+		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/translateDocument', async (context) => {
+	const { documentData, targetLang } = await context.req.json();
+	console.log('initial data:', documentData, targetLang);
+
+	// Generate a summary of the document
+	const summaryResponse = await context.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
+	console.log('summary res', summaryResponse);
+
+	// Translate the summary into another language
+	const response = await context.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	});
+	console.log(response);
+
+	// Return the translated response
+	return new Response(JSON.stringify(response));
+});
+
+export default app;
